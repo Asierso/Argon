b420207fe46a6e5edcb6aa75a0750e99a0abea7f
Asierso <asierso2000@gmail.com>
2020-12-02 23:47:04 +0100

    0

--- Upload.sh
+++ Upload.sh
@@ -2,8 +2,9 @@ echo "Update Commit:"
 read commit
 git init
 git add .
-git rm "Upload.sh"
+git rm Upload.sh
 git commit -m "$commit"
 git remote add origin "https://github.com/Asierso/Argon.git"
 echo "Credendials:"
 git push -u origin master
+echo "Code update to branch master"
